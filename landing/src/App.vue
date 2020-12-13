2ee5e7c3d0b12ad2d99bd771f93d4498372bb5a1
Michie1001 <michieugwu@gmail.com>
2020-12-13 16:35:27 +0100

    added fonts

--- landing/src/App.vue
+++ landing/src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Landing msg="Michelle"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Landing from './components/Landing.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Landing
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@font-face {
+  font-family: 'special_eliteregular';
+  src: url('./assets/specialelite-regular-webfont.woff2') format('woff2'),
+       url('./assets/specialelite-regular-webfont.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+}
+.landingGrid{
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: 1fr 20vh 50vh 1fr;
+  align-items: center;
+
+  .name{
+    grid-column: 2 / -1;
+    grid-row: 2 / 3;
+    font-family: 'special_eliteregular';
+    font-weight: 400;
+  }
+  .caricature{
+    grid-row: 3 / 4;
+  }
+  .aboutMe{
+    grid-row: 3 / 4;
+  }
+  .contactLinks{
+    grid-row: 4 / -1;
+  }
 }
 </style>
---
+++ landing/src/components/Landing.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="hello">
+    <div class="landingGrid">
+      <h1 class="name">{{ msg }}</h1>
+      <div class="caricature">Colorful hair</div>
+      <div class="aboutMe">
+
+        <ul class="whoYou">
+          <li>Frontend Engineer</li>
+          <li>Storyboarder</li>
+          <li>Cartoonist</li>
+          <li>Aspiring Animator</li>
+          <li>Writer</li>
+          <li>Traveller</li>
+          <li>Literal fish</li>
+          <li>Mostly spotted gaming</li>
+          <li>Also stares at the sky a lot</li>
+        </ul>
+
+      </div>
+      <div class="contactLinks">
+        <a href="">Github</a>
+        <a href="">LinkedIn</a>
+        <a href="">Drive</a>
+        <a href="">Instagram</a>
+        <a href="">Twitter</a>
+      </div>
+    </div>
+      <!-- <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Landing',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
