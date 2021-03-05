8f4299649bb8abbbf4cab01002e2e3e47648075b
Michie1001 <michieugwu@gmail.com>
2021-03-05 09:03:10 +0100

    better write up

--- portfolio/src/components/Landing.vue
+++ portfolio/src/components/Landing.vue
@@ -165,20 +165,6 @@
 
 
     data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
       experience: [
         {
           role: 'Frontend Engineer',
@@ -284,10 +270,10 @@
       messageToShow: '',
       messages: [
         'make things beautiful',
-        'engineer frontend code',
+        'write code',
         'animate',
-        'draw',
-        'write'
+        'draw sometimes',
+        'deal in fiction'
       ],
       show: 'false'
     }),
