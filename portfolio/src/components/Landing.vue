4b5f2b35042d9368cc90e89ae4d7d4f4f9808759
Michie1001 <michieugwu@gmail.com>
2021-03-08 01:32:16 +0100

    Gooey footer fail

--- portfolio/src/components/Landing.vue
+++ portfolio/src/components/Landing.vue
@@ -162,8 +162,37 @@
       </v-row>
 
     </v-container>
-    <v-bottom-navigation>
-    </v-bottom-navigation>
+    <!-- <v-bottom-navigation class="footer">
+      <div class="bubbles"
+        v-for="i < 128;"
+        :key="i">
+        <div class="bubble" style="`--size:${2+Math.random()*4}rem; --distance:${6+Math.random()*4}rem; --position:${-5+Math.random()*110}%; --time:${2+Math.random()*2}s; --delay:${-1*(2+Math.random()*2)}s;`">{{i}}</div>
+      </div>
+      <div class="content">
+        <div>
+            <div><b>Eldew</b><a href="#">Secuce</a><a href="#">Drupand</a><a href="#">Oceash</a><a href="#">Ugefe</a><a href="#">Babed</a></div>
+
+            <div><b>Spotha</b><a href="#">Miskasa</a><a href="#">Agithe</a><a href="#">Scesha</a><a href="#">Lulle</a></div>
+
+            <div><b>Chashakib</b><a href="#">Chogauw</a><a href="#">Phachuled</a><a href="#">Tiebeft</a><a href="#">Ocid</a><a href="#">Izom</a><a href="#">Ort</a></div>
+
+            <div><b>Athod</b><a href="#">Pamuz</a><a href="#">Vapert</a><a href="#">Neesk</a><a href="#">Omemanen</a></div>
+        </div>
+        <div>
+          <a class="image" href="https://codepen.io/z-" target="_blank" style="background-image: url(&quot;https://s3-us-west-2.amazonaws.com/s.cdpn.io/199011/happy.svg&quot;)"></a>
+          <p>©2019 Not Really</p>
+        </div>
+      </div>
+      <svg style="position:fixed; top:100vh">
+        <defs>
+          <filter id="blob">
+              <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur"></feGaussianBlur>
+              <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="blob"></feColorMatrix>
+              <feComposite in="SourceGraphic" in2="blob" operator="atop"></feComposite>
+          </filter>
+        </defs>
+      </svg>
+    </v-bottom-navigation> -->
   </div>
 </template>
 
