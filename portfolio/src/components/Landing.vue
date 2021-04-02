def7aefb45fc6fd106777ed09620497a6dee3c3c
Michie1001 <michieugwu@gmail.com>
2021-04-02 13:33:26 +0100

    made postscript into another component

--- portfolio/src/components/Landing.vue
+++ portfolio/src/components/Landing.vue
@@ -66,60 +66,9 @@
           ></v-img>
       </div>
 
-      <div class="postscript">
-        <h2>By Michelle</h2>
-        <p>All illustrations by Michelle</p>
-        <div class="contactMe">
-          <p>Send me an email, let's work together.<br/>
-          <b>michieugwu@gmail.com</b></p>
-        </div>
-      </div>
+      <postScript />
 
-      <div class="wave-effect">
-        <!-- Gotten the waves code from Daniel Österman on codepen, great stuff -->
-        <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-        viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
-        <defs>
-          <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
-        </defs>
-        <g class="parallax">
-          <use xlink:href="#gentle-wave" x="48" y="0" fill="#e0e0e0" />
-          <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.8)" />
-          <use xlink:href="#gentle-wave" x="48" y="5" fill="#f1e2e1" />
-          <use xlink:href="#gentle-wave" x="48" y="7" fill="#cdd2e3" />
-        </g>
-        </svg>
-      </div>
-
-      <footer>
-        <div class="contacts-section">
-          <div class="contacts"
-            v-for="reach in contact"
-            :key="reach.id">
-              <a :href="reach.link">
-                <h5>
-                  <i :class="reach.icon"></i>
-                  <br>
-                  <!-- {{reach.name}} -->
-                </h5>
-            </a>
-          </div>
-        </div>
-
-        <div class="footer-logo">
-          <v-img            
-            dark
-            class="imgg"
-            alt="Michelle's logo"
-            contain
-            width="30px"
-            height="30px"
-            :src="require('../assets/imgs/profileAvatar.svg')"
-          >
-          </v-img>
-          <small>&copy; Copyright 2021, Michelle Ugwu</small> 
-        </div>
-      </footer>
+      <Footer />
 
       <v-btn
         v-on:click="toTop"
@@ -144,6 +93,8 @@
 <script>
   import gridArea from './GridArea.vue'
   import workSection from './WorkSection.vue'
+  import postScript from './PostScript.vue'
+  import Footer from './Footer.vue'
   import * as ext from '../assets/extras.js'
 
   export default {
@@ -156,37 +107,12 @@
     components: {
       gridArea,
       workSection,
+      postScript,
+      Footer,
     },
 
 
     data: () => ({
-      contact: [
-        {
-          icon: 'fab fa-linkedin-in',
-          name: 'LinkedIn',
-          link: 'www.linkedin.com/in/michelle-ugwu',
-        },
-        {
-          icon: 'fab fa-instagram',
-          name: 'Instagram',
-          link: 'https://www.instagram.com/michie_1001/',
-        },
-        {
-          icon: 'fab fa-github',
-          name: 'Github',
-          link: 'https://github.com/Michie1001',
-        },
-        {
-          icon: 'fab fa-stack-overflow',
-          name: 'Stack Overflow',
-          link: 'https://stackoverflow.com/users/story/6276301',
-        },
-        {
-          icon: 'fab fa-twitter',
-          name: 'Twitter',
-          link: 'https://twitter.com/Michie1001',
-        },
-      ],
       
       messageToShow: `Hello ${ext.message}`,
       // onPageEnd: `${ext}`,
---
+++ portfolio/src/components/PostScript.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="postscript">
+    <h2>By Michelle</h2>
+    <p>All illustrations by Michelle</p>
+    <div class="contactMe">
+      <p>Send me an email, let's work together.<br/>
+      <b>{{email}}</b></p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      email: "michieugwu@gmail.com",
+    }),
+  }
+</script>
