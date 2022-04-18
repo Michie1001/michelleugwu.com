2ed2885955c95401bbc5978575c2f565b3f2650a
michelle <michelle@dukka.com>
2022-04-18 17:40:41 +0100

    refactor: moved the jumbotron into a module of its own

---
+++ portfolio/src/components/Jumbotron.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <div class="roundbg big"></div>
+        <nav class="about">    
+            <div class="roundbg only-dktp"></div>
+            <div class="roundbg small"></div>
+            <h1>
+            Michelle Ugwu
+            </h1>
+        </nav>
+    </div>
+</template>
--- portfolio/src/components/Landing.vue
+++ portfolio/src/components/Landing.vue
@@ -2,14 +2,7 @@
   <div class="landing">
 
     <div class="main-content" v:on:scroll.native="">
-      <div class="roundbg big"></div>
-      <nav class="about">    
-        <div class="roundbg only-dktp"></div>
-        <div class="roundbg small"></div>
-        <h1>
-          Michelle Ugwu <!-- (watch thi https://youtu.be/RIPdc88dZRI?list=WL to arrange my resume) -->
-        </h1>
-      </nav>
+      <jumbotron/>
       <div class="curved-bar">
       </div>
 
@@ -25,18 +18,6 @@
           ></v-img>
       </div>
 
-      <!-- <div v-for="(message, i) in messages" :key="i">
-        <vue-typed-js
-          
-          :stringsElement="'message'"
-          >
-          <h2 class="main-heading">
-            <span class="typing messages"></span>, <br>
-            Obsessed with the ocean, fiction, space, animals and art
-          </h2>
-        </vue-typed-js>
-      </div> -->
-
       <vue-typed-js
         :strings="['artist', 'writer', 'creative', 'designer', 'animator', 'frontend engineer']"
         :typeSpeed="80"
@@ -47,7 +28,6 @@
 
         >
         <h2 class="main-heading">
-          <!-- <p>{{messageToShow}}</p> -->
           <span class="typing messages"></span><br>
           Obsessed with the ocean, fiction, space, animals and art
         </h2>
@@ -122,7 +102,6 @@
     data: () => ({
       
       messageToShow: `Hello ${ext.message}`,
-      // onPageEnd: `${ext}`,
       messages: [
         'make things beautiful',
         'write code',
@@ -132,10 +111,6 @@
       ],
       show: 'false',
     }),
-
-    //Adding the .native up top or adding the created and destroyed
-    //fix my scroll problem just fine either way
-    //I chose less 
 
     created() {
       window.addEventListener('scroll', this.onPageEnd);
