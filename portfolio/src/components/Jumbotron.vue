6ed7a73b55f8d9c5ff27b2670d9e1acda802549b
michelle <michelle@dukka.com>
2022-04-18 19:06:38 +0100

    added buttons for artworks and projects

--- portfolio/src/components/Jumbotron.vue
+++ portfolio/src/components/Jumbotron.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
         <div class="roundbg big"></div>
+
+        <div class="navButtons">
+            <button class="navButtons_individual">To Artworks</button>
+            <button class="navButtons_individual float-right">To Projects</button>
+        </div>
+
         <nav class="about">    
             <div class="roundbg only-dktp"></div>
             <div class="roundbg small"></div>
