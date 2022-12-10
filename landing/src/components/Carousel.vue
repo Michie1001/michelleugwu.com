842b84dffbd667223fc32be1577010ec6373fe58
Michie1001 <michieugwu@gmail.com>
2022-12-10 00:56:48 +0000

    Revert "Removed unused folder"
    This reverts commit 327c1950b6509d4f903e0d4b98157de57c9f969a.

---
+++ landing/src/components/Carousel.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="carousel">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Carousel',
+    methods: {
+      
+    }
+  }
+</script>
---
+++ landing/src/components/CarouselSlide.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div v:show="visibleSlide === index" class="carousel-slide">
+      <slot>
+        <img :src="slide" />
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default{
+    props: ['visibleSlide', 'index', 'slide'],
+    data(){
+
+    }
+  }
+</script>
