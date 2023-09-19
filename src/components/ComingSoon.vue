349d3517995bbd3beedf5e2e60debfec61825bf8
Michelle <michieugwu@gmail.com>
2023-09-19 18:11:11 +0100

    Coming soon

---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,27 @@
+<script setup>
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+    <div>
+        This site is being reconstructed...check back in a couple of days
+        <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+        <img src='/src/assets/tansparentProfileAvatar.png'/>
+    </div>
+</template>
+
+<style scoped>
+div{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 auto;
+}
+img {
+    width: 4rem;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import ComingSoon from '../components/ComingSoon.vue';
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <ComingSoon />
+  </main>
+</template>
